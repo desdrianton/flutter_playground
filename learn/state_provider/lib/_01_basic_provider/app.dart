2972f722b0a236517846ac836ea3ec220e22e6f9
desdrianton <desdrianton@gmail.com>
2022-09-24 23:16:34 +0700

    Basic Provider

--- learn/state_provider/lib/_01_basic_provider/app.dart
+++ learn/state_provider/lib/_01_basic_provider/app.dart
@@ -1,18 +1,17 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:state_provider/_01_basic_provider/counter_widget.dart';
 
 class BasicProviderApp extends StatelessWidget {
   const BasicProviderApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CounterModel(),
-      child: MaterialApp(
-        title: 'Basic Provider App',
-        home: MyPage(),
-      ),
+    log('=== BasicProviderApp.build');
+
+    return MaterialApp(
+      home: MyPage(),
     );
   }
 }
@@ -22,69 +21,10 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    log('=== MyPage.build');
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Counter value: '),
-            const SizedBox(
-              height: 50,
-            ),
-            _counterInfo(),
-            const SizedBox(
-              height: 50,
-            ),
-            Row(
-              children: [_decreaseButton(context), _increaseButton(context)],
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: CounterWidget(),),
     );
-  }
-
-  Widget _counterInfo() {
-    return Consumer<CounterModel>(
-      builder: (context, counter, child) {
-        return Text(
-          '${counter.value}',
-          style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-        );
-      },
-    );
-  }
-
-  Widget _decreaseButton(BuildContext context) {
-    return OutlinedButton(
-      onPressed: () {
-        context.read<CounterModel>().decrease();
-      },
-      child: const Text('Decrease'),
-    );
-  }
-
-  Widget _increaseButton(BuildContext context) {
-    return OutlinedButton(
-      onPressed: () {
-        context.read<CounterModel>().increase();
-      },
-      child: const Text('Increase'),
-    );
-  }
-}
-
-class CounterModel with ChangeNotifier {
-  int value = 0;
-
-  void increase() {
-    value++;
-    notifyListeners();
-  }
-
-  void decrease() {
-    value--;
-    notifyListeners();
   }
 }
---
+++ learn/state_provider/lib/_01_basic_provider/counter_widget.dart
@@ -0,0 +1,101 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class _CounterModel with ChangeNotifier {
+  int value = 0;
+
+  void increase() {
+    value++;
+    notifyListeners();
+  }
+
+  void decrease() {
+    value--;
+    notifyListeners();
+  }
+}
+
+class CounterWidget extends StatelessWidget {
+  CounterWidget() : super(key: UniqueKey());
+
+  @override
+  Widget build(BuildContext context) {
+    log('=== CounterWidget.build');
+
+    return ChangeNotifierProvider(
+      create: (context) => _CounterModel(),
+      builder: (context, child) {
+        log('=== ChangeNotifierProvider.builder');
+
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [_DecreaseButton(), const Spacer(), _CounterInfo(), const Spacer(), _IncreaseButton()],
+        );
+      },
+    );
+  }
+}
+
+class _CounterInfo extends StatelessWidget {
+  _CounterInfo() : super(key: UniqueKey());
+
+  @override
+  Widget build(BuildContext context) {
+    log('=== CounterInfo.build');
+
+    return Consumer<_CounterModel>(
+      builder: (context, counter, child) {
+        log('=== Consumer.builder');
+
+        return Text(
+          '${counter.value}',
+          style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+        );
+      },
+    );
+  }
+}
+
+class _DecreaseButton extends StatelessWidget {
+  _DecreaseButton() : super(key: UniqueKey());
+
+  @override
+  Widget build(BuildContext context) {
+    log('=== DecreaseButton.build');
+
+    var button = OutlinedButton(
+      onPressed: () {
+        context.read<_CounterModel>().decrease();
+      },
+      child: const Text('-'),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: button,
+    );
+  }
+}
+
+class _IncreaseButton extends StatelessWidget {
+  _IncreaseButton() : super(key: UniqueKey());
+
+  @override
+  Widget build(BuildContext context) {
+    log('=== IncreaseButton.build');
+
+    var button = OutlinedButton(
+      onPressed: () {
+        context.read<_CounterModel>().increase();
+      },
+      child: const Text('+'),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: button,
+    );
+  }
+}
